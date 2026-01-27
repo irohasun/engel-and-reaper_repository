97de123663e86b98687b22e481159d5f279d3c1b
irohasun <nadasinkageryukiichi@gmail.com>
2026-01-27 21:20:20 +0900

    fix(release): bump version to 1.0.1 (build 10)

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: 'Angel & Reaper',
     slug: 'angel-reaper-game',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
